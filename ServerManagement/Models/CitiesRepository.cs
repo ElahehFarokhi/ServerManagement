c837bf63d56796b29708a1d64a7978c61eaa683b
elaheh farokhi <farokhi@ptcvep.net>
2024-06-11 14:17:31 +0330

    Add project files.

---
+++ ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> cities =
+        [
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        ];
+
+        public static List<string> GetCities() => cities;
+
+    }
+}
